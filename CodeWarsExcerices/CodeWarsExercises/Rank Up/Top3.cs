2e81f61fa8f4043020516fb30f2bc5cd4c52289b
anotidamubaiwa <anotida.mubaiwa@krs.co.za>
2024-06-27 10:28:56 +0200

    Top 3

--- CodeWarsExcerices/CodeWarsExercises/Rank Up/Top3.cs
+++ CodeWarsExcerices/CodeWarsExercises/Rank Up/Top3.cs
@@ -5,21 +5,41 @@ namespace CodeWarsExercises.Rank_Up;
 using System.Collections.Generic;
 using System.Linq;
 
-public static class TopThree
+public static partial class TopThree
 {
     public static List<string> Top3(string sentence)
     {
-        var words = sentence.Split(" ").ToList();
+        var words = SplitSentenceUsingSpecialChar().Split(sentence).ToList();;
         words.RemoveAll(word=> word.Equals(" ")||word.Equals("")||word.Equals("  ")||word.Equals("   "));
 
         var wordCount = new Dictionary<string, int>();
-        foreach (var newWord in from word in words select Regex.Replace(word, @"[^0-9a-zA-Z']+|[/]", "") into newWord where newWord is not (" " or "") where !wordCount.TryAdd(newWord.ToLower(), 1) select newWord)
+        foreach (var word in words)
         {
-            wordCount[newWord.ToLower()] += 1;
+            var newWord = CheckIfWordHasChars().Replace(word, "");
+            if (newWord is not (" " or ""))
+            {
+                if (!wordCount.TryAdd(newWord.ToLower(), 1))
+                {
+                    wordCount[newWord.ToLower()] += 1;
+                }
+            }
         }
 
-        var top3 = wordCount.OrderByDescending(key => key.Value).ThenByDescending(key => key.Key).Select(wordNumber => wordNumber.Key).Where(w=>!Regex.IsMatch(w, @"^[^0-9a-zA-Z]+$")).ToList();
+        var top3 = wordCount
+            .OrderByDescending(key => key.Value)
+            .ThenByDescending(key => key.Key)
+            .Select(wordNumber => wordNumber.Key)
+            .Where(w=>!MyRegex().IsMatch(w)).ToList();
 
         return top3.Take(3).ToList();
     }
+
+    [GeneratedRegex(@"^[^0-9a-zA-Z]+$")]
+    private static partial Regex MyRegex();
+    
+    [GeneratedRegex(@"[^0-9a-zA-Z']+|[/]")]
+    private static partial Regex CheckIfWordHasChars();
+    
+    [GeneratedRegex("[^a-zA-Z0-9']+")]
+    private static partial Regex SplitSentenceUsingSpecialChar();
 }
